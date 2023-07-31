9664ffac836112566bf0d3f15cf493730756b3d5
islam-42161 <mdsazzadul.islam.42161@gmail.com>
2023-07-31 22:12:39 +0600

    chance splash icon

--- screens/test/Test.jsx
+++ screens/test/Test.jsx
@@ -1,10 +1,26 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import EmojiPicker from "../../components/EmojiPicker";
 
 export default function Test({ navigation }) {
+  const [isModalVisible, setIsModalVisible] = useState(true);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text onPress={() => navigation.goBack()}>Go back</Text>
-    </View>
+    // <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    //   <Text onPress={() => navigation.goBack()}>Go back</Text>
+    // </View>
+    <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+      {/* A list of emoji component will go here */}
+      <Text>;)</Text>
+    </EmojiPicker>
   );
 }
